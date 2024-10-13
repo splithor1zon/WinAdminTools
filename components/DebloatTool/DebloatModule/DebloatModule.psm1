e76c171c93113238678dd598726c0eeee8371993
splithor1zon <splithor1zon@protonmail.ch>
2024-10-13 16:58:49 +0200

    DebloatTool : implement Get-Profile function

--- components/DebloatTool/DebloatModule/DebloatModule.psm1
+++ components/DebloatTool/DebloatModule/DebloatModule.psm1
@@ -1,5 +1,43 @@
-function Load-Profile {
+<#
+.SYNOPSIS
+    Get-Profile function is used to load a json debloat profile.
+.PARAMETER ProfilePath
+    Path to the debloat profile file.
+#>
+function Get-Profile {
+    param(
+        [string]$ProfilePath
+    )
 
+    if (-not (Test-Path $ProfilePath)) {
+        return $null
+    }
+
+    $DebloatProfile = Get-Content -Path $ProfilePath -Raw | ConvertFrom-Json
+    return $DebloatProfile
 }
 
-Export-ModuleMember -Function Load-Profile
+#TODO: Implement the Remove-Apps function
+function Remove-Apps {
+    param(
+        [string]$a
+    )
+
+    $DebloatProfile = Get-Profile -ProfilePath $ProfilePath
+
+    if ($DebloatProfile -eq $null) {
+        Write-Warning "Profile not found."
+        return
+    }
+
+    $DebloatProfile.Apps | ForEach-Object {
+        $App = $_
+        $App | ForEach-Object {
+            $AppName = $_
+            Write-Host "Removing $AppName..."
+            Get-AppxPackage -Name $AppName | Remove-AppxPackage
+        }
+    }
+}
+
+Export-ModuleMember -Function Get-Profile
